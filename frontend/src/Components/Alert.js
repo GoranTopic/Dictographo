24b01397c453d4d4e05fd3da078c1ebaab39910d
telix <telegonicaxx@live.com>
2020-12-16 20:11:12 -0800

    fixed minor bug in error msg

--- frontend/src/Components/Alert.js
+++ frontend/src/Components/Alert.js
@@ -10,27 +10,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  */
 
 function AlertContainer({state, dispatchState}) {
-
 		/* functions that return an error mesage depending onthe state */
-		const errorMsg = () =>  
-				<Alert variant="danger" dismissible 
-						onClose={() => dispatchState({type: 'DISSMISS_ERROR'})} >
-						<Alert.Heading>Oh Snap!</Alert.Heading>
-						{state.isWordNotFound? //if the there is not words found
-						<p>Looks like <b>
-								{ state.wordsNotFound.map(word => <i>"{word},"</i> ) }
-						</b>in our dictionary yet.</p> : <></>}
-						{state.isPathNotFound? //if there is a path not found
-						<p>Looks like there is no path between: <b> 
-								{ state.pathsNotFound.map(
-										path => <i>"{path.first}, {path.last}"</i> ) }
-						</b></p> : <></> }
-						}
-				</Alert>
-
-
 		if (state.isError) {
-				return errorMsg();
+				return(
+						<Alert variant="danger" dismissible 
+								onClose={() => dispatchState({type: 'DISSMISS_ERROR'})} >
+								<Alert.Heading>Oh Snap!</Alert.Heading>
+								{state.isWordNotFound? //if the there is not words found
+								<p>Looks like <b>
+										{ state.wordsNotFound.map(word => <i>"{word},"</i> ) }
+								</b>in our dictionary yet.</p> : <></>}
+								{state.isPathNotFound? //if there is a path not found
+								<p>Looks like there is no path between: <b> 
+										{ state.pathsNotFound.map(
+												path => <i>"{path.first}, {path.last}"</i> ) }
+								</b></p> : <></> }
+						</Alert>)
 		}else{
 				return <></>
 		}
