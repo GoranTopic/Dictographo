51ff9b9a50800638aba6b53d99adc20265386f74
telix <telegonicaxx@live.com>
2020-12-21 23:36:57 -0800

    added hook for detecting keys

--- frontend/src/Components/SearchSuggestion/SearchSuggestion.js
+++ frontend/src/Components/SearchSuggestion/SearchSuggestion.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { API_ENDPOINT }  from "../../myConfig";
+import useKeypress from '../../hooks/useKeypress';
 import styles from './SearchSuggestion.css'; 
 
 /*
@@ -9,94 +10,34 @@ import styles from './SearchSuggestion.css';
  * for predicting  what the user is going to type
  */
 
-import Autosuggest from 'react-autosuggest';
-
-
-const filterSuggestions = suggestions => {
-		return suggestions.filter( suggestion => /\s/.test(suggestion))
-};
-
-const getSuggestionValue = suggestion => suggestion;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
-		<div>
-				{suggestion.w_id}
-		</div>
-);
-
-class Example extends React.Component {
-		constructor() {
-				super();
-				this.state = {
-						value: '',
-						suggestions: []
-						    
-				};
-				  }
-
-		onChange = (event, { newValue }) => {
-				this.setState({
-						value: newValue 
-				})
-		};
-		
-		onSuggestionsFetchRequested = async ({ value }) => {
-				if(value.length > 2){
-						console.log(value);
-						fetch(API_ENDPOINT + query_search + value)
-								.then(result => result.json()) //unpack suggestions
-								.then(suggestions => { console.log(suggestions); return suggestions })
-								.then(suggestions => filterSuggestions(suggestions))
-								.then(suggestions => this.setState({
-										suggestions: suggestions,
-										value: value })
-								).catch((err) => console.log(err));
-				}
-		};
-
-		onSuggestionsClearRequested = () => {
-				this.setState({
-						      suggestions: []
-				});
-				  
-		};
-
-		render() {
-				const { value, suggestions  } = this.state;
-
-				// Autosuggest will pass through all these props to the input.
-				const inputProps = {
-						placeholder: 'dog cat',
-						value,
-						onChange: this.onChange
-				};
-
-				// Finally, render it!
-				return (
-						<Autosuggest
-								suggestions={suggestions}
-								onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-								onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-								getSuggestionValue={getSuggestionValue}
-								renderSuggestion={renderSuggestion}
-								inputProps={inputProps}
-						/>
-				);
-		}
-}
-
-let query_search = 'querysearch/'
+let query_search = 'querysearch/' //add this to myconfig
 
 function SuggestionsContainer(props){
-	/* tahek a child fro a input an add suggestions to it */
+		/* tahek a child fro a input an add suggestions to it */
 		// handle the change by seting the state variable to 
 		let state = props.state
 		let dispatchState = props.dispatchState;
 		const activeSuggestion = 1;
 		const [suggestions, setSuggestions] = useState([])	
 		const [selected, setSetselected] = useState(0)	
-				
+	
+
+
+		useKeypress('ArrowDown', () => {
+				setSetselected(setSetselected - 1);
+				console.log(selected);
+		});
+
+		useKeypress('ArrowUp', () => {
+				setSetselected(setSetselected + 1);
+				console.log(selected);
+		});
+
+
+
+		useKeypress('Enter', () => console.log("key pressed"));
+
+
 		const filterSuggestions = suggestions => 
 				suggestions.filter( suggestion => /\s/.test(suggestion))
 
@@ -111,7 +52,6 @@ function SuggestionsContainer(props){
 				});	
 				setSuggestions([]); // reset suggestions
 		}
-
 
 		useEffect(() => {
 				/* query server for search suggestionsa
@@ -130,12 +70,13 @@ function SuggestionsContainer(props){
 						.catch((err) => console.log(err));
 		}, [state.searchTerm, dispatchState])
 
+
 		const SuggestionList = () =>
 				<ul class="suggestions">
 						{ suggestions.map(
 								(suggestion, index) => {  
 										let className;
-										if (index === activeSuggestion) 
+										if (index === selected) 
 												className = styles.active
 										return  <li 
 												key={index} 
@@ -147,13 +88,10 @@ function SuggestionsContainer(props){
 						)}
 				</ul>
 
-		return <div className="input">
-				{props.children}
-				{state.searchTerm.length > 2? <SuggestionList/>: <></> }
-		</div>
+						return <div className="input">
+								{props.children}
+								{state.searchTerm.length > 2? <SuggestionList/>: <></> }
+						</div>
 }
-
-				//<Example/>
-
 
 export default SuggestionsContainer;
